a99074c483eae7fe3be725f1cfc49dc74f2377fa
henrical <bloodshotdawn55@gmail.com>
2015-03-08 13:47:43 -0700

    cleaned up comments

--- proj/main.cpp
+++ proj/main.cpp
@@ -29,36 +29,5 @@ int main()
   }
   
   list_print(graph);
-  
-  //std::cout << edge_num<< std::endl;
-  
-  
-  
-  
-  
-  
-  //std::cout << *i[0] << std::endl << *i[1] << std::endl;
-  
-  /* user interface TODO
-  std::string input_line;
-  std::cin >> input_line;
-  
-  std::istringstream(input_line) >> i
-  std::cout << i << std::endl;
-  */
-  
-  /*
-  Node* n1 = new Node(1);
-  
-  list_t graph = list_init(6);
-  list_insert_edge(graph, 2, 4);
-  list_insert_edge(graph, 2, 5);
-  list_insert_edge(graph, 2, 6);
-  list_insert_edge(graph, 1, 5);
-  list_insert_edge(graph, 5, 6);
-  list_insert_edge(graph, 6,6);
-  n1 = graph->list[1];
-  
-  list_print(graph);*/
 }
 
