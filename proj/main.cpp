73a1e0f0dbe21c4e464cf11eee467d730a23a637
henrical <bloodshotdawn55@gmail.com>
2015-03-13 16:22:19 -0700

    bfs initialization

--- proj/main.cpp
+++ proj/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <stdio.h>
 #include <limits>
+#include <cstring>
 #include "graph.h"
 #include "node.h"
 
+#define NIL -1
+#define WHITE 0
+#define GREY 1
+#define BLACK 2
+#define INFINITY std::numeric_limits<int>::max()
+
 void breadth_first_search(list_t graph, int orig)
 {
-  int color[graph->graph_size];
-  int predecessor[graph->graph_size];
-  int distance[graph->graph_size];
+  int size = graph->graph_size;
+  int i;
   
-  memset(color, 
+  int color[size];
+  int predecessor[size];
+  int distance[size];
   
+  //intialize color[] to 0
+  memset(color, WHITE, sizeof(color));
+  //initialize predecessor[] to NIL
+  memset(predecessor, NIL, sizeof(predecessor));
+  
+  //initialize size[] to infinity
+  for(i=0; i<size; i++)
+  {
+    distance[i] = INFINITY;
+  }
+    
 }
 
 
@@ -39,6 +58,8 @@ int main()
       list_insert_edge(graph, temp1, temp2);
   }
   
-  list_print(graph);
+  //list_print(graph);
+  
+  breadth_first_search(graph,orig_node);
 }
 
