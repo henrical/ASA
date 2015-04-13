81cd10d7855228e55f25ebc89db1cef974b097b5
henrical <bloodshotdawn55@gmail.com>
2015-04-13 02:54:22 -0700

    proj2

---
+++ proj2/main.c
@@ -0,0 +1,7 @@
+#include "vertex.h"
+#include "edge.h"
+
+int main()
+{
+	return 0;
+}
---
+++ proj2/vertex.h
@@ -0,0 +1,9 @@
+#ifndef _VERTEX_H
+#define _VERTEX_H
+
+typedef struct vertex{
+	int value;
+	edge_t[] edges;
+} *vertex_t
+
+#endif _VERTEX_H
