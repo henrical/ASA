b40f8785d614f792d972468ca1c25588fe025195
henrical <bloodshotdawn55@gmail.com>
2015-04-15 07:21:25 -0700

    restructuring

---
+++ proj2/Graph.java
@@ -0,0 +1,60 @@
+public class Graph {
+
+	private Vertex list[];
+	private int size;
+
+	public Graph(int size)
+	{
+		int i;
+
+		list = new Vertex[size];
+		this.size = size;
+
+		for(i=0; i<size; i++)
+			list[i] = new Vertex(i+1);
+	}
+
+	public int getSize()
+	{
+		return size;
+	}
+
+	/**
+	 * Returns the Vertex with the given value.
+	 * Values are [1,...,size]	
+	 */
+	private Vertex getVertexByValue(int value)
+	{
+		return list[value-1];
+	}
+
+	/**
+	 * Adds an edge of weight 'weight' between 'orig' and 'dest'
+	 */
+	void addEdge(int orig, int dest, int weight)
+	{
+		Vertex temp_orig = getVertexByValue(orig);
+		Vertex temp_dest = getVertexByValue(dest);
+		
+		temp_orig.addEdge(weight,temp_dest);
+	}
+
+	void print()
+	{
+		int i;
+
+		System.out.println("_____________________________");
+		System.out.println("------PRINTING GRAPH---------");
+		System.out.println("_____________________________");
+
+		for(i=0; i<size; i++)
+		{
+			list[i].print();
+		}
+
+		System.out.println("_____________________________");
+		System.out.println("_____________________________");
+		System.out.println("_____________________________");
+	}
+	
+}
---
+++ proj2/Vertex.java
@@ -0,0 +1,59 @@
+import java.util.Vector;
+
+public class Vertex {
+
+	private final int value;
+	private Vector<Edge> edges;	
+
+	public Vertex()
+	{
+		value = -1;
+		edges = new Vector<Edge>();
+	}
+
+	public Vertex(int val)
+	{
+		this.value = val;
+		edges = new Vector<Edge>();
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	public Edge[] getEdges()
+	{
+		Edge[] result = new Edge[edges.size()];
+		int i;
+
+		for(i=0; i<edges.size(); i++)
+		{
+			result[i] = edges.get(i);
+		}
+
+		return result;
+	}
+
+	public void addEdge(int weight, Vertex destination)
+	{
+		edges.add(new Edge(weight,destination));
+	}
+
+	/**
+	 * Prints the weight and destination of all connected edges.
+	 */
+	void print()
+	{
+		int i;
+
+		System.out.println("++++ Vertex " + getValue() + "++++");
+
+		for(i=0; i<edges.size(); i++)
+		{
+			edges.get(i).print();
+		}
+	}
+
+
+}
